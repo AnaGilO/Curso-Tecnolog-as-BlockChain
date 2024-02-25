10e7cf935ead386861fef77d51e260eafc3584cf
Ana Gil <gilorozcoana@gmail.com>
2024-02-25 11:25:39 +0100

    Update PrácticaBanco.sol
    Funcionalidades adicionales

--- PrácticaBanco.sol
+++ PrácticaBanco.sol
@@ -14,10 +14,25 @@ pragma solidity >=0.7.0 <0.9.0;
 // FUNCIONES INTERMEDIAS
 // El contrato del banco puede llevar una cantidad de ETH precargada en el momento de la creación del
 // contrato. Esta cantidad no pertenece a ninguna de las cuentas de los depositarios, obviamente.
+// Añadir la restricción del "coeficiente de caja"
 
 contract Banco {
     mapping (address => uint256) deudores;
+    mapping (address => uint256) prestamos;
     mapping (address => uint256) balances;
+
+    uint public coeficienteCaja;
+    uint256 public interes;
+
+    constructor() payable {
+        balances[msg.sender] += msg.value;
+        calcularCoeficienteCaja();
+        interes = 5; // Consideramos un interes del 5%  
+    }
+
+    function calcularCoeficienteCaja() public {
+        coeficienteCaja = (balances[msg.sender]*95)/100;
+    }
 
     function depositar() public payable {
         uint256 eth_recibido = msg.value;
@@ -38,7 +53,10 @@ contract Banco {
     }
 
     function solicitarPrestamo(uint256 cantidadPrestamo) public {
+        // Restricciones a cumplir para poder solicitar el prestamo
         require((consultarSaldoBanco() >= cantidadPrestamo), "El banco no dispone del saldo suficiente");
+        require((consultarSaldoBanco() >= coeficienteCaja), "El balance del banco no puede disminuir de su cinco por ciento total");
+        require((calcularMaximoPrestamo() <= cantidadPrestamo), "Se ha solicitado un prestamo de mas valor del permitido");
 
         // Al pedir el prestamo, aumenta simultaneamente la deuda y el balance del usuario
         deudores[msg.sender] += cantidadPrestamo;
@@ -47,15 +65,33 @@ contract Banco {
 
         // Añadimos el usuario a la lista de deudores
         deudores[msg.sender] += cantidadPrestamo;
+        // Apuntamos el importe del prestamo
+        prestamos[msg.sender] = cantidadPrestamo;
+    }
+
+    // Funcion adicional: El maximo prestamo posible a sacar del banco se considera de forma propocional al saldo total del que dispone el banco
+    // El valor calculado se devuelve como resultado de la función
+    function calcularMaximoPrestamo() private view returns (uint256){ 
+        // Consideramos el valor máximo como el 10 por ciento del saldo del banco 
+        return (balances[address(this)] * 10)/100;
     }
 
     function pagarPrestamo() public payable {
-        require(msg.value <= deudores[msg.sender], "El usuario aún no puede pagar su deuda");
-        
+        require(msg.value <= (deudores[msg.sender] + calcularInteres(msg.sender)), "El usuario aun no puede pagar su deuda");
+        // El banco recibe la devolución del prestamo y el interes del mismo como beneficio
+        balances[address(this)] += msg.value;
+        balances[address(this)] += calcularInteres(msg.sender);
+        calcularCoeficienteCaja();
+        deudores[msg.sender] -= (msg.value + calcularInteres(msg.sender));
+    }
+
+    function calcularInteres(address deudor) private view returns (uint256) {
+        // Calculamos el interes que le corresponde a este usuario
+        require(prestamos[deudor] >= 0, "Este usuario no tiene ningun prestamo pendiente de pago");
+        return (prestamos[deudor] * interes)/100;
     }
 
     function consultarDeudas(address direccionDeudor) public view returns (uint256) {
         return deudores[direccionDeudor];
     }
-
 }
